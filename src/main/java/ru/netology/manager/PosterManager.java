e1ace51b7438102993591f96c28cfb14f9758c03
Timur Kiselev <snygens@mail.ru>
2020-08-18 15:56:21 +0300

    add branch layers

--- src/main/java/ru/netology/manager/PosterManager.java
+++ src/main/java/ru/netology/manager/PosterManager.java
@@ -2,38 +2,37 @@ package ru.netology.manager;
 
 import lombok.Getter;
 import ru.netology.domain.PurchaseItem;
+import ru.netology.repository.PosterRepository;
+
 public class PosterManager {
     @Getter
     private int films = 10;
-    private PurchaseItem[] items = new PurchaseItem[0];
+    private PosterRepository repository;
 
-    public PosterManager(){
-
+    public PosterManager(PosterRepository repository) {
+        this.repository = repository;
     }
-    public PosterManager(int films){
-        if (films > 0) {
+
+    public PosterManager() {
+    }
+    public PosterManager(int films, PosterRepository repository) {
+        this.repository = repository;
+        if (films > 0)
             this.films = films;
-        }
+    }
+    public void add(PurchaseItem film) {
+        repository.save(film);
     }
 
-    public void add(PurchaseItem item) {
-        // создаём новый массив размером на единицу больше
-        int length = items.length + 1;
-        PurchaseItem[] tmp = new PurchaseItem[length];
-        System.arraycopy(items, 0, tmp, 0, items.length);
-        int lastIndex = tmp.length - 1;
-        tmp[lastIndex] = item;
-        items = tmp;
-    }
+    public PurchaseItem[] getShowLast() {
+        PurchaseItem[] film = repository.findAll();
 
-    public PurchaseItem[] getAll() {
-        int lengthDisplayFilm = Math.min(items.length, films);
+        int lengthDisplayFilm = Math.min(film.length, films);
         PurchaseItem[] result = new PurchaseItem[lengthDisplayFilm];
         for (int i = 0; i < result.length; i++) {
             int index = result.length - i - 1;
-            result[i] = items[index];
+            result[i] = film[index];
         }
         return result;
     }
-
 }
--- src/test/java/ru/netology/manager/PosterManagerTestNotExist.java
+++ src/test/java/ru/netology/manager/PosterManagerTestNotExist.java
@@ -1,5 +1,6 @@
+
 package ru.netology.manager;
-import org.junit.jupiter.api.BeforeEach;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -8,31 +9,27 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import ru.netology.domain.PurchaseItem;
 import ru.netology.repository.PosterRepository;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.doReturn;
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 @ExtendWith(MockitoExtension.class)
-class PosterManagerOneFilmsTest {
+class PosterManagerTestNotExist {
+
     @Mock
     private PosterRepository repository;
     @InjectMocks
     private PosterManager manager;
-    PurchaseItem first = new PurchaseItem(1, 1, "first", "ganer", false);
-
-    @BeforeEach
-    void setUp() {
-        manager.add(first);
-    }
 
     @Test
-    public void shouldDisplayOneFilm(){
-        PurchaseItem[] returned = new PurchaseItem[]{first};
+    public void shouldDisplayIfNoFilm(){
+        PurchaseItem[] returned = new PurchaseItem[]{};
         doReturn(returned).when(repository).findAll();
 
         PurchaseItem[] actual = manager.getShowLast();
-        PurchaseItem[] expected = new PurchaseItem[]{first};
+        PurchaseItem[] expected = new PurchaseItem[]{};
 
         assertArrayEquals(expected, actual);
 
     }
+
 }
